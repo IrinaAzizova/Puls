1b04b16d3312bd061a83b9da2f2407a10bb1ec5b
Azizova Irina <i_r_i_n_a_nikitina@mail.ru>
2022-10-28 20:45:04 +0300

    change font

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,46 @@
-const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-
-function server() {
-    browserSync.init({
-        server: {
-            baseDir: "src"
-        }
-    });
-};
-
-function styles() {
-    return src('src/sass/*.+(scss|sass)')
+ 
+function runServer() {
+  browserSync.init({
+    server: {
+      baseDir: 'src',
+    },
+  });
+}
+ 
+function sassToCss() {
+  return src('src/sass/*.+(scss|sass)')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(
       rename({
         prefix: '',
-        suffix: '.min'
+        suffix: '.min',
       })
     )
     .pipe(
       autoprefixer({
-        cascade: false
+        cascade: false,
       })
     )
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(dest('src/css'))
     .pipe(browserSync.stream());
-};
-
-function watchUpdates() {
-    watch('src/sass/*.+(scss|sass)', styles);
-    watch('src/*.html').on('change', browserSync.reload);
-};
-
+}
+ 
+function watchChanges() {
+  watch('src/sass/**/*.+(scss|sass)', sassToCss);
+  watch('src/*.html').on('change', browserSync.reload);
+}
+ 
 function defaultTask() {
-    server();
-    styles();
-    watchUpdates();
-};
-
-exports.default = defaultTask; 
+  runServer();
+  sassToCss();
+  watchChanges();
+}
+ 
+exports.default = defaultTask;
