8c52eea8747f68481a9f0a0b50f67f6773e6a5be
Azizova Irina <i_r_i_n_a_nikitina@mail.ru>
2022-11-07 22:23:32 +0300

    layout of the catalog section, a script for tabs and a script for switching information in the product card

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const slider = ItcSlider.getOrCreateInstance('.itc-slider');
+
+	/* _____ slider connection _____ */
+	const slider = ItcSlider.getOrCreateInstance('.itc-slider');
+
+	/* _____ product card animation _____ */
+	const cardWrapper = document.querySelectorAll('.catalogue-card__wrapper'),
+		  moreLinks = document.querySelectorAll('.catalogue-card__more'),
+		  backLinks = document.querySelectorAll('.catalogue-card__back');
+
+	moreLinks.forEach((eachLink, i) => {
+	  	eachLink.addEventListener('click', (event) => {
+			event.preventDefault();
+			cardWrapper[i].classList.add('catalogue-card__wrapper_active');
+	  	});
+  	});
+
+	backLinks.forEach((eachLink, i) => {
+		eachLink.addEventListener('click', (event) => {
+		  event.preventDefault();
+		  cardWrapper[i].classList.remove('catalogue-card__wrapper_active');
+		});
+	});
+
+	/* _____ tabs _____ */
+	const tabs = document.querySelectorAll('.catalogue__tab'),
+		  catalogueCards = document.querySelectorAll('.catalogue-card');
+
+	tabs.forEach((eachTab, i) => {
+		eachTab.addEventListener('click', () => {
+			tabs.forEach(tab => {
+				tab.classList.remove('catalogue__tab_active');
+			});
+			catalogueCards.forEach(eachCard => {
+				if (eachCard.dataset.tab != tabs[i].dataset.tab) {
+					eachCard.style.display = 'none';
+				} else {
+					eachCard.style.display = 'block';
+				}
+			});
+			tabs[i].classList.add('catalogue__tab_active');			
+		});
+	});
 });
